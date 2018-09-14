98bf39c8c31b1f73cf83188cfde080553972b7e4
PennController <39499242+PennController@users.noreply.github.com>
2018-09-13 23:20:30 -0400

    creation

--- data_includes/example_data.js
+++ data_includes/example_data.js
@@ -3,17 +3,11 @@ PennController.ResetPrefix(null);
 
 var items = [
     ["trial", "PennController", PennController(
-        newText("test sentence", "The fish swim in a tank which is perfectly round")
-            .print()        // A test sentence
+        newText("green", "How warm is the color green to you?")
+            .print()
         ,
-        newImage("competitor", "http://files.lab.florianschwarz.net/ibexfiles/PennController/SampleTrials/1fishSquareTank.png")
-            .print()        // An image with 1 fish that swims in a square tank
-        ,
-        newImage("target", "http://files.lab.florianschwarz.net/ibexfiles/PennController/SampleTrials/2fishRoundTank.png")
-            .print()        // An image with 2 fish that swim in a round tank
-        ,
-        newSelector("tank") // We indicate that target+competitor belong to a selection group
-            .settings.add( getImage("target") , getImage("competitor") )
-            .wait()         // On hold until target or competitor is selected
+        newScale("judgment", 5) // 5-point scale
+            .print()
+            .wait()             // Validate upon choice
     )]
 ];
